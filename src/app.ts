787ad7df0182932d676466aa6a3869d916959a1d
Al-Amin49 <alamin496832@gmail.com>
2024-08-20 23:56:52 +0600

    prettier eslint setup complete

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import cors from 'cors';
+
 const app = express()
 
+//middlewares
+app.use(express.json())
+app.use(cors())
 
-app.get('/', (req, res) => {
+
+app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
 })
 
