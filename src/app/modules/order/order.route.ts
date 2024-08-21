47f1cc09a21a6d585104e4f38b415e496ddb8d16
Al-Amin49 <alamin496832@gmail.com>
2024-08-21 15:20:09 +0600

    deployed on vercel

--- src/app/modules/order/order.route.ts
+++ src/app/modules/order/order.route.ts
@@ -2,10 +2,10 @@ import express from 'express';
 import { OrderControllers } from './order.controller';
 
 const router = express.Router();
-router.get('/', OrderControllers.getOrdersByEmail);
+router.get('/email', OrderControllers.getOrdersByEmail);
+router.post('/', OrderControllers.createOrder);
 router.get('/', OrderControllers.getAllOrders)
 
-router.post('/', OrderControllers.createOrder);
 
 
 
