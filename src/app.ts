2dc4108e11b23261641e783d839e496c333f28d4
Al-Amin49 <alamin496832@gmail.com>
2024-08-22 16:41:32 +0600

    deploy issue solved

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,21 @@ import express, { Request, Response } from 'express'
 import cors from 'cors';
 import { ProductRoutes } from './app/modules/product/product.route';
 import { OrderRoutes } from './app/modules/order/order.route';
+import notFound from './app/middlewares/notFound';
+
 
 const app = express()
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+  })
+);
+
+
 
 
 // application routes
@@ -15,8 +24,10 @@ app.use('/api/products', ProductRoutes);
 app.use('/api/orders', OrderRoutes);
 
 
+
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
 })
+app.use(notFound);
 
 export default app;
